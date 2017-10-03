478819f14316ef1ef7b5208a98bcc57903f5aec6
shi <itshilei@gmail.com>
2017-10-03 15:46:38 +0200

    Added redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import {View} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers';
+import {Header} from './components/common/Header';
+import LibraryList from './components/LibraryList';
 
 const App =  ()=>{
+
+    //This is the store we create with redux's createStore method
+    const store = createStore(reducers);
+
+    // Provider is given the store as a prop
     return(
-        <View>
-hi
-        </View>
+        <Provider store={store}>
+            <View>
+                <Header headerText='Tech Stack'/>
+                <LibraryList/>
+            </View>
+        </Provider>
+
     )
 }
 
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class LibraryList extends Component{
     render() {
+        console.log(this.props);
         return;
     }
 }
 
-export default LibraryList;
+const mapStateToProps = state =>{
+    console.log(state);
+    return{
+        libraries: state.libraries
+    }
+}
+
+export default connect(mapStateToProps)(LibraryList);
